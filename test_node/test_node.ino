d3c4827c8790a71da7800e41a09bc8e538d15be3
Unknown <paulamarinbanque@gmail.com>
2018-07-19 16:04:52 +0200

    server + sd

--- test_node/test_node.ino
+++ test_node/test_node.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+#define Pin 5 
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  int value = analogRead(Pin);
+  Serial.println(value);
+
+  delay(1000);
 }
